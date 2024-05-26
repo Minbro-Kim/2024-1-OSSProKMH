5898d838ec344710133b53b2d5f421977e770ee0
Minbro-Kim <ab000701@dgu.edu>
2024-05-26 19:48:29 +0900

    backend 기본설정수정

--- src/backend/src/test/java/com/backend/backend/BackendApplicationTests.java
+++ src/backend/src/test/java/com/backend/backend/BackendApplicationTests.java
@@ -1,10 +1,10 @@
-package com.backend;
+package com.backend.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class backendApplicationTests {
+class BackendApplicationTests {
 
 	@Test
 	void contextLoads() {
